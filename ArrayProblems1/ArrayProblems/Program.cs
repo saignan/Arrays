0095c46d0f06c57e2c287fcdaa6d1d54b8340eb2
saignan <saignankarrolla695@gmail.com>
2023-04-17 23:00:37 +0530

    [Saignan]Fix: initial Commit

---
+++ ArrayProblems1/ArrayProblems/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ArrayProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Table of Contents\r\n1) C# program to declare, initialize, input and print array elements\r\n2) C# program to print all negative elements in array\r\n3) C# program to find sum of array elements\r\n4) C# program to count negative elements in array\r\n5) C# program to find maximum and minimum element in array\r\n6) C# program to find second largest number in array\r\n7) C# program to count even and odd elements in array\r\n8) C# program to insert an element in array\r\n9) C# program to delete element from an array\r\n10) C# program to print all unique elements in array\r\n11) C# program to delete duplicate elements from array\r\n12) C# program to merge two sorted array\r\n13) C# program to find reverse of array\r\n14) C# program to search element in an array\r\n15) C# program to sort array in ascending or descending order\r\n16) C# program to add two matrices\r\n17) C# program to subtract two matrices\r\n18) C# program to multiply two matrices");
+            Console.WriteLine("Enter a value from 1 to 18:");
+            int input = int.Parse(Console.ReadLine());
+            switch (input)
+            {
+                
+            }
+        }
+    }
+}
---
+++ Demo/MyFirstProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
